3761594237a83d53182fff35d78a1ce13be3caae
landenlabs <landenlabs@gmail.com>
2024-12-19 19:52:56 -0500

    fix handling of . and ..

--- src/dirscan.cpp
+++ src/dirscan.cpp
@@ -335,8 +335,12 @@ size_t DirectoryScan::GetFilesInDirectory2(int depth)
                 continue;
             }
 
-            if (m_entryType != eFile &&
-               (FileData.cFileName[0] != '.' ||  isalnum(FileData.cFileName[1])))
+            // Bad if
+            //     cFileName[0] == '.' && cFilename[1] == '\0'
+            // or  cFilename[0] == '.' && cFilename[1] == '.' && cFilename[2] == '\0'
+            if (m_entryType != eFile 
+                && !(FileData.cFileName[0] == '.' && FileData.cFileName[1] == '\0')
+                && !(FileData.cFileName[0] == '.' && FileData.cFileName[1] == '.' && FileData.cFileName[2] == '\0'))
             {
                 if (m_recurse || depth < (int)m_dirFilters.size())
                 {
